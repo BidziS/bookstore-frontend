a6f0d070754ba265dd95e80cdd0f51ba0299a0aa
BidziS <danielcudnik93@gmail.com>
2017-07-18 15:10:41 +0200

    add basic packages for project

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+ 
+module.exports = {
+    entry: {
+        app: './src/app/appModule.js'
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            // { enforce: 'pre', test: /\.js$/, loader: "source-map-loader" },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [ {
+                    loader: 'html-loader',
+                    options: {
+                        minimize: false
+                    }
+                }   
+                ]
+            },
+                {
+                    test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/,
+                    loader: 'url-loader'
+                }
+        ]
+    },
+    devServer: {
+      port: 3000,
+      inline: true, //allows us to run automatic live code update
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new webpack.LoaderOptionsPlugin({
+            debug: true
+        })
+    ]
+};
